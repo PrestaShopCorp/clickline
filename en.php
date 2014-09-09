0c405ec2a0d5dbdbd5f1a42c844292870ea59f94
desarrollo2 <desarrollo2@clickline.com>
2014-09-09 12:55:02 +0200

    English translation

---
+++ en.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{clickline}prestashop>clickline_configuration_2fdfd506efea08144c0794c32ca8250a'] = 'Create an account';
+$_MODULE['<{clickline}prestashop>clickline_configuration_f4f70727dc34561dfde1a3c529b6205c'] = 'Configuration';
+$_MODULE['<{clickline}prestashop>clickline_configuration_5b6cf869265c13af8566f192b4ab3d2a'] = 'Documentation';
+$_MODULE['<{clickline}prestashop>clickline_configuration_db5eb84117d06047c97c9a0191b5fffe'] = 'Support';
+$_MODULE['<{clickline}prestashop>clickline_configuration_0fe272e7218a611a68be78b5506803a8'] = 'You must have an account on clickline.com';
+$_MODULE['<{clickline}prestashop>clickline_configuration_4343ebe1aae410bf19c89de1f5070218'] = 'Quit managing different accounts, Clickline is a shipping comparator integrated in PrestaShop.';
+$_MODULE['<{clickline}prestashop>clickline_configuration_9ecb53ac2891da8725a3d869f7ffba00'] = 'This module allows your customers to choose the carrier they want, and let you manage your shipments in peace.';
+$_MODULE['<{clickline}prestashop>clickline_configuration_68a3a5295917c00b26beab464e046513'] = 'For the module to work correctly you must create an account on Clickline, when shipping is also required to access the web a www.clickline.com to fund your account.';
+$_MODULE['<{clickline}prestashop>clickline_configuration_95c4f7d858d523454dd0ecdb77e6b0f3'] = 'As a client of Clickline also save up 5 percent in each of their shipments and:';
+$_MODULE['<{clickline}prestashop>clickline_configuration_aad961764b7dd5dc103e5130eacd34fa'] = 'Expedite the shipment process';
+$_MODULE['<{clickline}prestashop>clickline_configuration_00dd4aaea217f1ecbc68c7c8fab7e7dc'] = 'Check your orders status';
+$_MODULE['<{clickline}prestashop>clickline_configuration_2ea6adc442bf4aafda6ef1bd3e008277'] = 'Fund your prepaid account';
+$_MODULE['<{clickline}prestashop>clickline_configuration_b9704045634fea6ac9333e7e892ca78c'] = 'If you have any questions please contact us by email at comunicados@clickline.com or calling us on Monday through Friday October +34 902 10 80 902 from 9 am to 6 pm';
